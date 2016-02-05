8373a72ac771d2fddf3bce58c3747e60ba0b1ebf
Slawa Pidgorny <spidgorny@gmail.com>
2016-02-06 00:43:38 +0100

    - FIX calculated invoice price was always the same

--- class.t3lib_extMgm.php
+++ class.t3lib_extMgm.php
@@ -4,12 +4,12 @@ class t3lib_extMgm {
 
 	static function extPath($ext) {
 		if ($ext == 'sl_timetracking') {
-			return '';
+			return __DIR__.'/../';
 		}
 		if ($ext == 'sl_countusers') {
-			return 'sl_countusers/';
+			return __DIR__.'/../sl_countusers/';
 		}
-		return '../';
+		return __DIR__.'/../';
 	}
 
 	static function siteRelPath($ext) {
